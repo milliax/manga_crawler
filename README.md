0b0d14df6b5efd8057e23540e1be3df1718dca41
milliax <admin@milliax.me>
2022-04-24 23:55:02 +0800

    parsing good

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 This project is using selenium to crawl websites
 
-# Getting Started
+
+
+# prerequisites
 1. install selenium plugin which is used to control your web browser
 https://sites.google.com/chromium.org/driver/downloads
-
+2. install java 11
 
 ### Question
 1. No chinese in WSL
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-import json
 
 from browsermobproxy import Server
-
+import json
+from bs4 import BeautifulSoup
+import re
 
 if __name__ == "__main__":
 
     number = input("請輸入番號：")
     book = input("請輸入書號：")
 
-
     """ start up proxy server to collect data """
     server = Server("./browsermob-proxy/bin/browsermob-proxy")
     server.start()
-    proxy = server.create_proxy({'captureHeaders': True, 'captureContent': True, 'captureBinaryContent': True})
+    proxy = server.create_proxy(
+        {'captureHeaders': True, 'captureContent': True, 'captureBinaryContent': True})
 
     """ adding chrome options """
     #service_args = ["--proxy=%s" % proxy.proxy, '--ignore-ssl-errors=yes']
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     chrome_options.add_argument("ignore-certificate-errors")
 
     """ Open controlable google chrome """
-    driver = webdriver.Chrome("./chromedriver",options=chrome_options)
+    driver = webdriver.Chrome("./chromedriver", options=chrome_options)
 
     """ Open URL """
     driver.get(
@@ -33,9 +34,37 @@ if __name__ == "__main__":
 
     """ Entering target URL """
     button = driver.find_element_by_link_text("第 {book} 卷".format(book=book))
-    proxy.new_har() # start recording
+    # start recording
+    time.sleep(1)
+    proxy.new_har("new_har", options={'captureContent': True})
     button.click()
 
     """ wait for the requests """
     time.sleep(5)
-    print(proxy)
+    result = proxy.har
+    file_object = open("sample.txt",'a')
+    count = 20
+
+    press_link=False
+    link = ""
+
+    queue = []
+
+    for entry in result["log"]["entries"]:
+        #print(entry["response"]["content"])
+        pack = entry["response"]["content"]
+        #file_object.write(json.dumps(pack))
+        
+        if(pack["mimeType"] != "text/html"):
+            continue
+        
+        redirect_Message = "<head><title>Object moved</title></head>"
+        if(pack["text"][:39] == redirect_Message[:39]):
+            print(pack)
+            press_link = True
+            
+            datum = BeautifulSoup(pack["text"],"html.parser")
+            queue.append(datum.find_all("a")[0]["href"])
+            
+    root = queue[2]
+    print(root)
